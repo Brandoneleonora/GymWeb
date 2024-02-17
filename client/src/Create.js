32f085ccb4ec7ad43f6437e95088ca6514a0c493
Brandon Eleonora <brandoneleonora38@gmail.com>
2024-02-17 18:08:13 -0500

    fixed up more of the create modal feature

--- client/src/Create.js
+++ client/src/Create.js
@@ -1,17 +1,57 @@
-import React, { useState } from "react"
+import React, { createElement, useState } from "react"
 import white from  "./white.jpg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faImage } from '@fortawesome/free-solid-svg-icons'
+import { faX } from '@fortawesome/free-solid-svg-icons'
+
+
 function Create({ user, createModal, setCreateModal }){
-    const [image, setImage] = useState("")
+    const [image, setImage] = useState(true)
     const [typeOfPost, setPostType] = useState("")
 
+
+    const previewImage = (event) => {
+        const file = event.target.files[0]
+        const imageContainer = document.getElementById("previewImage")
+
+        if (file) {
+            const fileReader = new FileReader()
+            fileReader.onload = () => {
+                imageContainer.src = fileReader.result
+            }
+            fileReader.readAsDataURL(file)
+        }
+
+        setImage(!image)
+    }
+
+
+    const backButton = () => {
+        const input = document.getElementById("file")
+
+        if (input.files) {
+            setImage(!image)
+        }
+    }
+    
     return (
-        <div class="create-wrapper" onClick={() => setCreateModal(!createModal)}>
+        <div class="create-wrapper">
+        <button class={"create_exit"} onClick={() => setCreateModal(!createModal)}><span><FontAwesomeIcon icon={faX} size='xl' /></span></button>
             <div class="create-container">
                 <div class="create-header">
+                {image ? null : <button class="back_button" onClick={backButton}>Back</button>}
                     <h3>Share A Memory</h3>
+                    {image ? null : <button class="foward_button">Next</button>}
                 </div>
                 <div class="create-bottom">
-                    <button>Check Files</button>
+                    <div class="bottom-default">
+                        <input type="file" id="file" name="file"  accept="image/*" onChange={previewImage} hidden/>
+                        {image ? <img id="previewImage" src="#" alt="previewImage" hidden/> : <img id="previewImage" src="#" alt="previewImage"/> }
+                        <div class="create_default">
+                            {image && <span><FontAwesomeIcon icon={faImage} size='xl' /></span>}
+                            {image && <label for="file">Check Files</label>}
+                        </div>    
+                    </div>    
                 </div>
             </div>
         </div>
